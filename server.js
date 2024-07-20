a943c9745e381ea0fb0ef6c68608cf4ae847c479
samhitaveluri <121224422+samhitaveluri@users.noreply.github.com>
2024-07-20 12:59:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-const path = require("path");
+ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-const formatMessage = require("./utils/messages");
-const createAdapter = require("@socket.io/redis-adapter").createAdapter;
-const redis = require("redis");
-require("dotenv").config();
-const { createClient } = redis;
+const formatMessage = require("./utils/messages");  
 const {
   userJoin,
   getCurrentUser,
   userLeave,
-  getRoomUsers,
-} = require("./utils/users");
+  getRoomUsers
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -22,13 +18,7 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 const botName = "ChatCord Bot";
-
-(async () => {
-  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
-  await pubClient.connect();
-  subClient = pubClient.duplicate();
-  io.adapter(createAdapter(pubClient, subClient));
-})();
+ 
 
 // Run when client connects
 io.on("connection", (socket) => {
